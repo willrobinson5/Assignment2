d48058580f2c5da2152c904139963144c9704a4c
willrobinson5 <wcrobinson@hotmail.co.uk>
2017-04-21 13:55:37 +0100

    Saved the user informarion from the registration form into the underlying user table.

--- src/main/java/com/artistbase2/controller/UserController.java
+++ src/main/java/com/artistbase2/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.artistbase2.controller;
 
 import com.artistbase2.domain.User;
+import com.artistbase2.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by web on 19/04/17.
@@ -12,8 +16,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "/user")
 public class UserController {
+    @Autowired
+    UserService userService;
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
+
     public String registerView(Model model)
     {
         User user = new User();
@@ -21,4 +28,12 @@ public class UserController {
         return "register";
     }
 
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+//    @ResponseBody
+    public String register(Model model, @ModelAttribute("user") User user)
+    {
+        userService.save(user);
+        return "redirect:/";
+        //return "the user" + user.getFirstname();
+    }
 }
