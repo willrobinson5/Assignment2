4eb8ee91ce726321aadeaf90d38c4e4db11cbd66
willrobinson5 <wcrobinson@hotmail.co.uk>
2017-04-27 15:34:29 +0100

    Created a form to add an artist into the artist database. Created new artist repository interface.

--- src/main/java/com/artistbase2/domain/UserRepository.java
+++ src/main/java/com/artistbase2/domain/UserRepository.java
@@ -10,10 +10,12 @@ import java.util.List;
  */
 public interface UserRepository extends JpaRepository<User, Long> {
 
-//    List<User> findByFirstnameAndPassword(String accountname, String password);
+    List<User> findByFirstnameAndPassword(String accountname, String password);
+
     @Query("SELECT u FROM User u WHERE u.firstname=?1 AND u.password=?2")
     List<User> checkUserInput(String accountname, String password);
 
-
+    @Query("SELECT u FROM User u WHERE u.firstname like %?1% AND u.surname like %?2%")
+    List<User> searchUsers(String firstname, String surname);
 }
 
