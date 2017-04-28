8c5d55d60b915adbbf71c988772f62db1f3083a5
willrobinson5 <wcrobinson@hotmail.co.uk>
2017-04-28 13:56:46 +0100

    Problem fixed where the artists added into the table wouldn't save permanently. Can delete the artist from the artist table.

--- src/main/java/com/artistbase2/controller/ArtistController.java
+++ src/main/java/com/artistbase2/controller/ArtistController.java
@@ -7,11 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.List;
 
 /**
  * Created by web on 27/04/17.
@@ -43,6 +46,37 @@ public class ArtistController {
             return "artist/addArtist";
         }
         artistService.save(artist);
-        return "redirect:/";
+        return "artist/artistIndex";
+    }
+
+
+    @RequestMapping(value = "/artistIndex", method = RequestMethod.GET)
+    public String artistIndex(Model model, HttpSession session)
+    {
+        List<Artist> artist = artistService.findAll();
+        model.addAttribute("artist", artist);
+        return "artist/artistIndex";
+    }
+
+    @RequestMapping(value = "/artistUpdate/{artist}", method = RequestMethod.GET)
+    public String artistUpdateView(Model model, @PathVariable Artist artist)
+    {
+        model.addAttribute("artist", artist);
+        return "artist/artistUpdate";
+    }
+
+    @RequestMapping(value = "/artistUpdate", method = RequestMethod.POST)
+    public String update(Model model, @ModelAttribute("user") Artist artist)
+    {
+        artistService.save(artist);
+        return "artist/artistIndex";
+    }
+
+    @RequestMapping(value = "/delete/{artist}", method = RequestMethod.GET)
+//    @ResponseBody
+    public String deleteArtist(@PathVariable Artist artist)
+    {
+        artistService.delete(artist);
+        return "artist/artistIndex";
     }
 }
